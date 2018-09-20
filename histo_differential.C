877bb12005f5f97e1939d494bd7b801fa1cf6fdb
tkoiwai <koiwai@nex.phys.s.u-tokyo.ac.jp>
2018-09-20 13:13:40 +0900

    somehow modified

--- histo_differential.C
+++ histo_differential.C
@@ -5,6 +5,7 @@
 #include "TH1F.h"
 #include "TSystem.h"
 #include "TROOT.h"
+#include "TCanvas.h"
 
 
 void histo_differential(const char* histname){
@@ -22,6 +23,13 @@ void histo_differential(const char* histname){
     bin_content[i] = abs(h->GetBinContent(i+1) - h->GetBinContent(i));
     h_diff->SetBinContent(i,bin_content[i]);
   }
+
+  TCanvas *cc;
+  cc = new TCanvas("cc","cc",1200,400);
+  cc->Divide(2,1);
+  cc->cd(1);
+  h->Draw();
+  cc->cd(2);
   h_diff->Draw();
   
   
