a81eea2177a4653cd27125cdeb5930265ea6b878
tkoiwai <koiwai@nex.phys.s.u-tokyo.ac.jp>
2020-05-26 13:52:08 +0900

    cleaned up the code

--- vertex_frank.C
+++ vertex_frank.C
@@ -48,10 +48,11 @@ int main(int argc, char *argv[]){
 
   //---------------------------------
   //===== Load input MINOS file =====
-  TString infnameM = Form("/home/koiwai/analysis/rootfiles/minos/cal/cal_minos%04d.root",filenum);
+  //TString infnameM = Form("/home/koiwai/analysis/rootfiles/minos/cal/cal_minos%04d.root",filenum);
+  TString infnameM = Form("/home/koiwai/analysis/minos/Tracks_run_%04d.root",filenum);
   TFile   *infileM = TFile::Open(infnameM);
   TTree   *caltrM;
-  infileM->GetObject("caltrM",caltrM);
+  infileM->GetObject("tree",caltrM);
 
   //===== Input tree variables =====
   Int_t EventNumber_minos, RunNumber_minos;
@@ -61,9 +62,6 @@ int main(int argc, char *argv[]){
   vector<double> *p3 = 0;
   Int_t tracknum;
 
-  Int_t br56ca, br56sc, br54ca;
-  Int_t sa55ca, sa53ca, sa55k;
-  
   //===== SetBranchAddress =====
   caltrM->SetBranchAddress("EventNumber",&EventNumber_minos);
   caltrM->SetBranchAddress("RunNumber",&RunNumber_minos);
@@ -73,14 +71,7 @@ int main(int argc, char *argv[]){
   caltrM->SetBranchAddress("parFit3",&p2);
   caltrM->SetBranchAddress("parFit4",&p3);
   caltrM->SetBranchAddress("NumberTracks",&tracknum);
-  /*
-  caltrM->SetBranchAddress("br56ca",&br56ca);
-  caltrM->SetBranchAddress("br56sc",&br56sc);
-  caltrM->SetBranchAddress("br54ca",&br54ca);
-  caltrM->SetBranchAddress("sa55ca",&sa55ca);
-  caltrM->SetBranchAddress("sa55k",&sa55k);
-  caltrM->SetBranchAddress("sa53ca",&sa53ca);
-  */
+
   //------------------------------
   //===== Load input DC file =====
   TString infnameDC = Form("/home/koiwai/analysis/rootfiles/ana/mwdc/anaDC%04d.root",filenum);
@@ -94,11 +85,7 @@ int main(int argc, char *argv[]){
 
   Double_t BDC1_X, BDC1_Y;
   Double_t BDC2_X, BDC2_Y;
-  //Double_t BDC_X, BDC_Y, BDC_A, BDC_B;
   Double_t Target_X, Target_Y;
-
-  //Int_t BG_flag_dc;
-
   
   //===== SetBranchAddress =====
   anatrDC->SetBranchAddress("EventNumber",&EventNumber_dc);
@@ -108,13 +95,8 @@ int main(int argc, char *argv[]){
   anatrDC->SetBranchAddress("BDC1_Y",&BDC1_Y);
   anatrDC->SetBranchAddress("BDC2_X",&BDC2_X);
   anatrDC->SetBranchAddress("BDC2_Y",&BDC2_Y);
-  //anatrDC->SetBranchAddress("BDC_X",&BDC_X);
-  //anatrDC->SetBranchAddress("BDC_Y",&BDC_Y);
-  //anatrDC->SetBranchAddress("BDC_A",&BDC_A);
-  //anatrDC->SetBranchAddress("BDC_B",&BDC_B);
   anatrDC->SetBranchAddress("Target_X",&Target_X);
   anatrDC->SetBranchAddress("Target_Y",&Target_Y);
-  //anatrDC->SetBranchAddress("BG_flag",&BG_flag_dc);
 
   //===== Load input Beam file =====
   TString infnameB = Form("/home/koiwai/analysis/rootfiles/ana/beam/ana_beam%04d.root",filenum);
@@ -147,37 +129,8 @@ int main(int argc, char *argv[]){
   caltrM->AddFriend(anatrB);
   caltrM->AddFriend(anatrS);
   
-  /*
+
   //===== Load cut files =====
-  TFile *brcuts = new TFile("/home/koiwai/analysis/cutfiles/BRpid.root","");
-
-  TCutG *cbr56ca = (TCutG*)brcuts->Get("br56ca");
-  TCutG *cbr55ca = (TCutG*)brcuts->Get("br55ca");
-  TCutG *cbr54ca = (TCutG*)brcuts->Get("br54ca");
-  TCutG *cbr53ca = (TCutG*)brcuts->Get("br53ca");
-  TCutG *cbr52ca = (TCutG*)brcuts->Get("br52ca");
-  
-  
-  TFile *sacuts = new TFile("/home/koiwai/analysis/cutfiles/SApid.root","");
-  //  TCutG* sacut[50];
-  //int j=0;
-
-  TCutG *csa56ca = (TCutG*)sacuts->Get("sa56ca");
-  TCutG *csa55ca = (TCutG*)sacuts->Get("sa55ca");
-  TCutG *csa54ca = (TCutG*)sacuts->Get("sa54ca");
-  TCutG *csa53ca = (TCutG*)sacuts->Get("sa53ca");
-  TCutG *csa52ca = (TCutG*)sacuts->Get("sa52ca");
-
-  TCutG *csa55k = (TCutG*)sacuts->Get("sa55k");
-  */
-  /*  
-  sacut[j] = (TCutG*)sacuts->Get("sa56ca");sacut[j++]->SetName("csa56ca");
-  sacut[j] = (TCutG*)sacuts->Get("sa55ca");sacut[j++]->SetName("csa55ca");
-  sacut[j] = (TCutG*)sacuts->Get("sa54ca");sacut[j++]->SetName("csa54ca");
-  sacut[j] = (TCutG*)sacuts->Get("sa53ca");sacut[j++]->SetName("csa53ca");
-  sacut[j] = (TCutG*)sacuts->Get("sa52ca");sacut[j++]->SetName("csa52ca");
-  */
-  //sacuts->Close();
 
   //===== Load .dat files =====
   
@@ -188,7 +141,7 @@ int main(int argc, char *argv[]){
   TTree   *tr    = new TTree("tr","tr");
 
   //===== Declare const.s =====
-  Double_t d = 2235.31; // [mm]: BDC1 - MINOS entrance.
+  Double_t d = 2310.31; // [mm]: BDC1 - MINOS entrance.
   Double_t dBDC = 999.53 ; // [mm]: BDC1 - BDC2.
   Double_t PI = TMath::Pi();
 
@@ -212,12 +165,6 @@ int main(int argc, char *argv[]){
   
   Double_t theta2p;
 
-  /*
-  Int_t br56ca, br55ca, br54ca, br53ca, br52ca;
-  Int_t sa56ca, sa55ca, sa54ca, sa53ca, sa52ca, sa55k;
-  */
-  
-  
   //===== Create tree Branch =====
   tr->Branch("runnum",&runnum);
   tr->Branch("eventnum",&eventnum);
@@ -230,45 +177,10 @@ int main(int argc, char *argv[]){
   tr->Branch("vertexR",&vertexR);
   tr->Branch("vertexTheta",&vertexTheta);
   tr->Branch("vertexPhi",&vertexPhi);
-  /*
-  tr->Branch("p0a",&p0a);
-  tr->Branch("p1a",&p1a);
-  tr->Branch("p2a",&p2a);
-  tr->Branch("p3a",&p3a);
-  tr->Branch("p0b",&p0b);
-  tr->Branch("p1b",&p1b);
-  tr->Branch("p2b",&p2b);
-  tr->Branch("p3b",&p3b);
-  
-  tr->Branch("beta",&beta);
-  tr->Branch("alpha",&alpha);
-  tr->Branch("B",&B);
-  tr->Branch("C",&C);
-
-  tr->Branch("xa",&xa);
-  tr->Branch("ya",&ya);
-  tr->Branch("za",&za);
-  tr->Branch("xb",&xb);
-  tr->Branch("yb",&yb);
-  tr->Branch("zb",&zb);
-  */
 
   
   tr->Branch("theta2p",&theta2p);
-  /*
-  tr->Branch("br56ca",&br56ca);
-  tr->Branch("br56sc",&br56sc);
-  //tr->Branch("br55ca",&br55ca);
-  tr->Branch("br54ca",&br54ca);
-  //tr->Branch("br53ca",&br53ca);
-  //tr->Branch("br52ca",&br52ca);
-  //tr->Branch("sa56ca",&sa56ca);
-  tr->Branch("sa55ca",&sa55ca);
-  //tr->Branch("sa54ca",&sa54ca);
-  tr->Branch("sa53ca",&sa53ca);
-  //tr->Branch("sa52ca",&sa52ca);
-  tr->Branch("sa55k",&sa55k);
-  */
+
   //===== Begin LOOP =====
   int nEntry = caltrM->GetEntries();
  for(int iEntry=0;iEntry<nEntry;iEntry++){
@@ -321,20 +233,7 @@ int main(int argc, char *argv[]){
 
     theta2p = Sqrt(-1);
 
-    /*
-    br56ca = 0;
-    br55ca = 0;
-    br54ca = 0;
-    br53ca = 0;
-    br52ca = 0;
-    sa56ca = 0;
-    sa55ca = 0;
-    sa54ca = 0;
-    sa53ca = 0;
-    sa52ca = 0;
-    sa55k  = 0;
-    */
-    
+   
     //=== Calc ===
     p0beam = BDC1_X + bdc_dx/dBDC*d;
     p1beam = bdc_dx/dBDC;
@@ -383,41 +282,16 @@ int main(int argc, char *argv[]){
     ya = p2a + p3a*za;
     yb = p2b + p3b*zb;
 
-
-    //cout << endl;
-    //cout << "za = " << za << endl;
-    //cout << "zb = " << zb << endl;
-
-    
     vertexX = (xa + xb)/2.;
     vertexY = (ya + yb)/2.;
     vertexZ = (za + zb)/2.;
 
-    //cout << "z = " << vertexZ << endl;
-    //cout << "truck num: " << tracknum << endl; 
-    //cout << endl;
-    
     vertexR     = Sqrt(vertexX*vertexX + vertexY*vertexY + vertexZ*vertexZ);
     vertexTheta = ACos(vertexZ/vertexR) *180./PI;
     vertexPhi   = ACos(vertexX/vertexR/Sin(vertexTheta)) *180./PI;
 
     theta2p = ACos((p1a*p1b + p3a*p3b +1)/(Sqrt(p1a*p1a+p3a*p3a+1)*Sqrt(p1b*p1b+p3b*p3b+1)))*180./PI;
 
-    /*
-    //===== BG cut =====
-    if(cbr56ca->IsInside(aoqBR,zetBR)) br56ca = 1;
-    if(cbr55ca->IsInside(aoqBR,zetBR)) br55ca = 1;
-    if(cbr54ca->IsInside(aoqBR,zetBR)) br54ca = 1;
-    if(cbr53ca->IsInside(aoqBR,zetBR)) br53ca = 1;
-    if(cbr52ca->IsInside(aoqBR,zetBR)) br52ca = 1;
-    if(csa56ca->IsInside(aoqSA,zetSA)) sa56ca = 1;
-    if(csa55ca->IsInside(aoqSA,zetSA)) sa55ca = 1;
-    if(csa54ca->IsInside(aoqSA,zetSA)) sa54ca = 1;
-    if(csa53ca->IsInside(aoqSA,zetSA)) sa53ca = 1;
-    if(csa52ca->IsInside(aoqSA,zetSA)) sa52ca = 1;
-    if(csa55k->IsInside(aoqSA,zetSA))  sa55k  = 1;
-    */
-    
     tr->Fill();
   }//for LOOP end
   outf->cd();
